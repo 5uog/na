f745ebc93f2534722ec37ab5dc5bf95d0cd9ea2e
ٴ <104566003+ivwy@users.noreply.github.com>
2022-10-09 13:05:33 +0900

    -

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -6,23 +6,3 @@ function scrollUp(){
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
-
-
-/*===== jquery scroll for mobile =====*/
-$(document).ready(function(){
-    // Add smooth scrolling to all links with the class scroll
-    $("a.scroll").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-            var hash = this.hash;
-            
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-});
