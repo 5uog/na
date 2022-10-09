e85908e00802955dee85e9620012eb85e8342fe1
ٴ <104566003+ivwy@users.noreply.github.com>
2022-10-09 11:40:27 +0900

    fixed project grid

--- assets/js/popup/popup.js
+++ assets/js/popup/popup.js
@@ -2,6 +2,9 @@
 $(function (){
     $('.popup-youtube').magnificPopup({
         type: 'iframe',
+        midClick: true, // to open the link in new tab
         removalDelay: 200,
+        closeOnBgClick: true, // nothing changed
+        enableEscapeKey: true, // nothing changed
     });
 });
