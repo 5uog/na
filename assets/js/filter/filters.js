89881453e5aa31e4916782935a5c40853701f88b
ٴ <104566003+ivwy@users.noreply.github.com>
2022-10-11 23:16:29 +0900

    projects filter

---
+++ assets/js/filter/filters.js
@@ -0,0 +1,29 @@
+// Portfolio Item Filter
+const filterContainer = document.querySelector('.projects-filter'),
+    filterBtns = filterContainer.children,
+    totalFilterBtn = filterBtns.length,
+    portfolioItems = document.querySelectorAll('.projects-item'),
+    totalPortfolioItem = portfolioItems.length;
+
+for (let i = 0; i < totalFilterBtn; i++) {
+    filterBtns[i].addEventListener('click', function () {
+        filterContainer.querySelector('.active').classList.remove('active');
+        this.classList.add('active');
+
+        const filterValue = this.getAttribute('data-filter');
+        for (let k = 0; k < totalPortfolioItem; k++) {
+            if (filterValue === portfolioItems[k].getAttribute('data-category')) {
+                portfolioItems[k].classList.remove('hide');
+                portfolioItems[k].classList.add('show');
+            } else {
+                portfolioItems[k].classList.remove('show');
+                portfolioItems[k].classList.add('hide');
+            }
+
+            if (filterValue === 'all') {
+                portfolioItems[k].classList.remove('hide');
+                portfolioItems[k].classList.add('show');
+            }
+        }
+    });
+}
