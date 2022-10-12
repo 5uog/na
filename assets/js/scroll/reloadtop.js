16cd6985e6dd6d9f735e0b49b58d3ad3ff21dd2d
ٴ <104566003+ivwy@users.noreply.github.com>
2022-10-12 22:07:26 +0900

    reload top

---
+++ assets/js/scroll/reloadtop.js
@@ -0,0 +1,9 @@
+// force scroll to the top
+history.scrollRestoration = 'manual';
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
